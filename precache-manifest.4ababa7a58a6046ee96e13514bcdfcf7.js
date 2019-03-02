edaa7b139e24bff8020b253d70c53cdcc1977758
Olamilekan <Asmovic4ril@yahoo.com>
2019-03-02 16:34:25 +0100

    Robot
    Rb

---
+++ precache-manifest.4ababa7a58a6046ee96e13514bcdfcf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4225fa1f1a44b1ad89e1",
+    "url": "/static/js/main.4225fa1f.chunk.js"
+  },
+  {
+    "revision": "2e612145e307e0586b3d",
+    "url": "/static/js/2.2e612145.chunk.js"
+  },
+  {
+    "revision": "4225fa1f1a44b1ad89e1",
+    "url": "/static/css/main.c18a7364.chunk.css"
+  },
+  {
+    "revision": "2e612145e307e0586b3d",
+    "url": "/static/css/2.8f1eb89b.chunk.css"
+  },
+  {
+    "revision": "0cc150ad4c72bc68183602f05cb963ae",
+    "url": "/index.html"
+  }
+];
